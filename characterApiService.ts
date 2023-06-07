4ac0128d5980a66159d2bd8b5c851f3db574dc21
Ethan22Lawrence <Ethan22Lawrence@users.noreply.github.com>
2023-06-06 17:00:37 -0700

    api base url update

--- characterApiService.ts
+++ characterApiService.ts
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const characterApi = createApi({
     reducerPath: "characterApi",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000/api"}),
+    baseQuery: fetchBaseQuery({baseUrl: "https://hero-battle-git-main-ethan22lawrence.vercel.app/api"}),
     endpoints: (builder) => ({
         getAllCharacters: builder.query({
             query: page => ({url: "getAllCharacters", params: {page}}),
